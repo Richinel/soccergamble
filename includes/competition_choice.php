38e2a8970f9bc30bdc335391557f9812c52c90c1
LAPTOP-GNV1OT6P\Richi <richinel_jarbandhan@hotmail.com>
2016-10-06 16:05:10 +0200

    End of the day commit (School)

--- includes/competition_choice.php
+++ includes/competition_choice.php
@@ -1,7 +1,11 @@
 <div class="comp_choice">
 	<div class="wrapper">
-		<div class="row centered">
-			<?php include('./controllers/fp_comp_controller.php'); ?>
-		</div>
+		<center><h3>Hoe werkt het?</h3></center>
+		<fieldset class="fieldset_800">
+		<legend>1. Kies een competitie</legend>	
+			<div class="row centered">
+				<?php include('./controllers/fp_comp_controller.php'); ?>
+			</div>
+		</fieldset>
 	</div>
 </div>
--- includes/fp_competitions.php
+++ includes/fp_competitions.php
@@ -1,7 +1,6 @@
 <div class="fp_competitions">
 	<div class="wrapper">
-		<h2>Recente wedstrijden</h2>	
-		<hr>
+		<center><h3>Recente wedstrijd?</h3></center>	
 		<div class="row centered blocks">	
 			<?php include('./controllers/ucl_match.php'); ?>
 		</div>
